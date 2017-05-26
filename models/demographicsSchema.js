6d185213437281d27d5ca6f551f0f532b8d0c1d3
chris-daynes <chrisdaynes@gmail.com>
2017-05-26 22:22:13 +1200

    moved the moved the demographics to there own file

---
+++ models/demographicsSchema.js
@@ -0,0 +1,20 @@
+
+  module.exports = {nhi: {
+        type: String,
+        validate: {
+            //the validator here needs to be a regex or something more precise
+            validator: (nhi) => nhi.length === 7,
+            message: "The NHI does not have seven digits"
+        },
+        required: [ true, 'NHI is required.']
+        },
+        firstName: String,
+        lastName: String,
+        dob: String,
+        age: Number,
+        address_street: String,
+        address_suburb: String,
+        address_city: String
+    }
+
+   
--- models/skin.js
+++ models/skin.js
@@ -1,19 +1,11 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const SkinSchema = new Schema({
-    nhi: {
-        type: String,
-        validate: {
-            //the validator here needs to be a regex or something more precise
-            validator: (nhi) => nhi.length === 7,
-            message: "The NHI does not have seven digits"
-        },
-        required: [ true, 'NHI is required.']
-    },
-    firstName: String,
-    lastName: String
-})
+const DemographicsSchema = require('./demographicsSchema')
+
+const SkinSchema = new Schema(
+    DemographicsSchema
+)
 
 const Skin = mongoose.model('skin', SkinSchema)
 
